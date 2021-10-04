6de7ad6c0bdf7541193eef78ab779bdcafa457ba
Oluwafemi <femikola23@gmail.com>
2021-10-04 17:43:44 +0400

    Update scripts

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   document.querySelector(".logo__title").classList.add("rotate");
 });
+
+const navToggle = document.querySelector('.dis-mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu')
+
+navToggle.addEventListener('click', closeMenu)
+function closeMenu() {
+  mobileMenu.style.display='none'
+}
