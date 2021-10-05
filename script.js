6baf7fa5e993b9256b00e784921bd2a76faa866d
ntazelo <ntazelo@gmail.com>
2021-10-05 12:58:48 +0200

    Add mobile navbar

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-
-
 document.addEventListener("DOMContentLoaded", function (event) {
   document.querySelector(".logo__title").classList.add("rotate");
 });
 
-const navToggle = document.querySelector('.dis-mobile-menu');
-const mobileMenu = document.querySelector('.mobile-menu')
-
-navToggle.addEventListener('click', closeMenu)
+const navToggle = document.querySelector(".dis-mobile-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const humbMenu = document.querySelector(".menu--humb");
+const section = document.querySelectorAll(".link-mobile-menu");
+navToggle.addEventListener("click", closeMenu);
 function closeMenu() {
-  mobileMenu.style.display='none'
+  mobileMenu.style.display = "none";
 }
+
+humbMenu.addEventListener("click", OpenMenu);
+function OpenMenu() {
+  mobileMenu.style.display = "block";
+}
+
+section.forEach((item) => {
+  item.addEventListener("click", closeMenu);
+});
