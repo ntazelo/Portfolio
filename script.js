24384057f5032c1af0f5a64402d14653ff0f249f
ntazelo <ntazelo@gmail.com>
2021-10-05 16:00:46 +0200

    Add project details object

--- script.js
+++ script.js
@@ -12,6 +12,45 @@ document.addEventListener('DOMContentLoaded', () => {
   modal.appendChild(contPrjctDet);
   contPrjctDet.appendChild(heading);
 });
+const projectInfo = {
+  project1: {
+    title: 'Tonic',
+    prj_info: ['Canopy', 'fas fa-circle color__circle', 'Baack End Dev', 'fas fa-circle color__circle', 2015],
+    url_img: './images/SnapshootPortfolio.png',
+    text_content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    languages: ['html', 'css', 'javascript'],
+    button_live: { url: '#', icon_class: 'fas fa-life-ring' },
+    button_source: { url: '#', icon_class: 'fab fa-github-square' },
+  },
+  project2: {
+    title: 'Tonic',
+    prj_info: ['Canopy', 'fas fa-circle color__circle', 'Baack End Dev', 'fas fa-circle color__circle', 2015],
+    url_img: './images/SnapshootPortfolio1.png',
+    text_content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    languages: ['html', 'css', 'javascript'],
+    button_live: { url: '#', icon_class: 'fas fa-life-ring' },
+    button_source: { url: '#', icon_class: 'fab fa-github-square' },
+  },
+  project3: {
+    title: 'Tonic',
+    prj_info: ['Canopy', 'fas fa-circle color__circle', 'Baack End Dev', 'fas fa-circle color__circle', 2015],
+    url_img: './images/SnapshootPortfolio2.png',
+    text_content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    languages: ['html', 'css', 'javascript'],
+    button_live: { url: '#', icon_class: 'fas fa-life-ring' },
+    button_source: { url: '#', icon_class: 'fab fa-github-square' },
+  },
+  project4: {
+    title: 'Tonic',
+    prj_info: ['Canopy', 'fas fa-circle color__circle', 'Baack End Dev', 'fas fa-circle color__circle', 2015],
+    url_img: './images/SnapshootPortfolio3.png',
+    text_content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    languages: ['html', 'css', 'javascript'],
+    button_live: { url: '#', icon_class: 'fas fa-life-ring' },
+    button_source: { url: '#', icon_class: 'fab fa-github-square' },
+  },
+};
+
 const navToggle = document.querySelector('.dis-mobile-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const humbMenu = document.querySelector('.menu--humb');
