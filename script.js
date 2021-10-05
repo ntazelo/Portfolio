7f75db7b47585fc8df293419e9fadfcdfb6e6259
ntazelo <ntazelo@gmail.com>
2021-10-05 14:07:18 +0200

    Add javascript linter

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  document.querySelector(".logo__title").classList.add("rotate");
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.logo__title').classList.add('rotate');
 });
-
-const navToggle = document.querySelector(".dis-mobile-menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-const humbMenu = document.querySelector(".menu--humb");
-const section = document.querySelectorAll(".link-mobile-menu");
-navToggle.addEventListener("click", closeMenu);
-function closeMenu() {
-  mobileMenu.style.display = "none";
-}
-
-humbMenu.addEventListener("click", OpenMenu);
+const navToggle = document.querySelector('.dis-mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const humbMenu = document.querySelector('.menu--humb');
+const section = document.querySelectorAll('.link-mobile-menu');
+function closeMenu() { mobileMenu.style.display = 'none'; }
+navToggle.addEventListener('click', closeMenu);
 function OpenMenu() {
-  mobileMenu.style.display = "block";
+  mobileMenu.style.display = 'block';
 }
-
+humbMenu.addEventListener('click', OpenMenu);
 section.forEach((item) => {
-  item.addEventListener("click", closeMenu);
+  item.addEventListener('click', closeMenu);
 });
