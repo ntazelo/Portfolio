0d4d164b677bd3d3d3ed6d48b62079a7c86e540b
ntazelo <ntazelo@gmail.com>
2021-10-05 15:09:52 +0200

    Add project detail pop up

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.logo__title').classList.add('rotate');
+  const modal = document.createElement('section');
+  const body = document.querySelector('body');
+  const contPrjctDet = document.createElement('div');
+  const heading = document.createElement('h1');
+  modal.classList.add('modal-pop-up');
+  contPrjctDet.classList.add('cont-prjct-det');
+  heading.classList.add('pop-heading-det');
+  heading.textContent = 'Tonic';
+  body.appendChild(modal);
+  modal.appendChild(contPrjctDet);
+  contPrjctDet.appendChild(heading);
 });
 const navToggle = document.querySelector('.dis-mobile-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
