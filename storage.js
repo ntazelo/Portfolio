602da8f001a8b9057e82841a707c7a4186e8d3bc
Oluwafemi <femikola23@gmail.com>
2021-10-08 16:56:26 +0400

    Update storage feature

--- storage.js
+++ storage.js
@@ -1,2 +1,7 @@
 const formData = { name: '', email: '', message: '' };
 
+const name=document.querySelector('.user-name');
+const email=document.querySelector('.user-email');
+const message=document.querySelector('.user-message');
+
+let dataSaved=localStorage.getItem('data')
