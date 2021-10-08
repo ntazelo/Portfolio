48f9725badaa7be90fa68aadfb46868ac43aaf6d
ntazelo <ntazelo@gmail.com>
2021-10-08 16:33:49 +0200

    Add and retrieve data from localstorage

--- storage.js
+++ storage.js
@@ -1,7 +1,39 @@
-const formData = { name: '', email: '', message: '' };
+const userName = document.querySelector('.user-name');
+const email = document.querySelector('.user-email');
+const message = document.querySelector('.user-message');
+window.addEventListener('load', () => {
+  const localData = JSON.parse(localStorage.getItem('data'));
+  if (localData) {
+    userName.value = localData.name;
+    email.value = localData.email;
+    message.value = localData.message;
+  }
+});
 
-const name=document.querySelector('.user-name');
-const email=document.querySelector('.user-email');
-const message=document.querySelector('.user-message');
+userName.addEventListener('input', () => {
+  const localData = JSON.parse(localStorage.getItem('data'));
+  const data = {
+    ...localData,
+    name: userName.value,
+  };
+  localStorage.setItem('data', JSON.stringify(data));
+});
 
-let dataSaved=localStorage.getItem('data')
+email.addEventListener('input', () => {
+  const localData = JSON.parse(localStorage.getItem('data'));
+  const data = {
+    ...localData,
+    email: email.value,
+  };
+
+  localStorage.setItem('data', JSON.stringify(data));
+});
+
+message.addEventListener('input', () => {
+  const localData = JSON.parse(localStorage.getItem('data'));
+  const data = {
+    ...localData,
+    message: message.value,
+  };
+  localStorage.setItem('data', JSON.stringify(data));
+});
